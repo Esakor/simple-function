268dc65821d8bab15e8cdab418527e8bbf420f1a
Esakor <100299697+Esakor@users.noreply.github.com>
2022-05-03 10:47:49 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,21 @@
 input.onButtonPressed(Button.A, function () {
+    number += -1
+    basic.showNumber(number)
+})
+input.onButtonPressed(Button.AB, function () {
     Countdown()
 })
+input.onButtonPressed(Button.B, function () {
+    number += 2
+    basic.showNumber(number)
+})
 function Countdown () {
-    for (let index = 0; index <= 15; index++) {
-        basic.showNumber(15 - index)
+    for (let index = 0; index <= number; index++) {
+        basic.showNumber(number - index)
     }
 }
+let number = 0
+number = 0
 basic.forever(function () {
 	
 })
